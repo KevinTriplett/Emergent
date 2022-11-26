c4d332092d62f1efb489003e6943b76eb6ccb88e
Kevin Triplett <kevin@mopacmedia.com>
2022-11-25 18:31:35 -0600

    change timezone to UTC

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Emergent
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Central Time (US & Canada)"
+    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "UTC"
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.admin_name = (Rails.env == 'production' ?
