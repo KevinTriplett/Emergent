48cb5fa781e0eeb5c8bfa0972aa7ca13446ab9f8
Kevin Triplett <kevin@mopacmedia.com>
2022-12-08 18:41:11 -0600

    remove authentication

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,10 +2,6 @@ module Admin
   class UsersController < ApplicationController
     layout "admin"
 
-    unless Rails.env.test?
-      http_basic_authenticate_with name: Rails.configuration.admin_name, password: Rails.configuration.admin_password
-    end
-  
     def index
       date = ("2022-11-18").to_date
       @users = User.order(request_timestamp: :desc).where('request_timestamp >= ?', date)
