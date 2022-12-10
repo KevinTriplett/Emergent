f90edadfd1d8fa921748c0b4a0d805890f0b2a58
Kevin Triplett <kevin@mopacmedia.com>
2022-12-09 19:47:12 -0600

    tell capistrano where passenger instreg is

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,9 @@ lock "~> 3.17.1"
 
 set :application, "Emergent"
 set :repo_url, "git@github.com:KevinTriplett/Emergent.git"
-
+set :passenger_environment_variables, {
+  'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/run/passenger-instreg'
+}
 # Default branch is :master
 set :branch, 'main'
 
