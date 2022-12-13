5d27255711f5edc2df1493ebb7bb388047f1449e
Kevin Triplett <kevin@mopacmedia.com>
2022-12-13 15:21:08 -0600

    a little more twiddling ;/

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  # sessions for authentication
+  get  '/enter', to: 'sessions#create'
+  post '/leave', to: 'sessions#destroy'
+
   post "/admin/users(/:id)/update_user", to: "admin/users#update_user", as: :admin_update_user
   post "/admin/users(/:id)/approve", to: "admin/users#approve_user", as: :admin_approve_user
 
