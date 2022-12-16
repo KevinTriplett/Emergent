e907b787c7946019afab056e3163f8346cb75416
Kevin Triplett <kevin@mopacmedia.com>
2022-12-15 22:26:28 -0600

    [bug] whenever schedule uses correct cap var

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,7 +12,7 @@
 
 set :output, "/home/deploy/Emergent/current/log/cron.log"
 
-if Rails.env.production?
+if 'production' == @environment
   every 1.day, at: "4:30am" do
     rake "ec:nm_crawl_all"
   end
