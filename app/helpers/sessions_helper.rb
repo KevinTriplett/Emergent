3836554c33bdbd81d8c9f96e5e364d5524962dff
Kevin Triplett <kt@kevintriplett.com>
2023-01-05 19:29:17 -0600

    remove unnecessary optimization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,11 +25,11 @@ module SessionsHelper
   def current_user
     return unless cookies[:session_token]
     session_token = verify_and_decrypt_cookie(:session_token)
-    @current_user ||= User.find_by_session_token(session_token)
+    user = User.find_by_session_token(session_token)
     # TODO: remove this after January 2022
-    cookies.permanent[:user_name] = @current_user.name
-    cookies.permanent[:user_id] = @current_user.id
-    @current_user
+    cookies.permanent[:user_name] = user.name
+    cookies.permanent[:user_id] = user.id
+    user
   end
 
   def signed_in?
