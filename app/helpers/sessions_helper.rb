b3a9d782585a5a756a92da15f29379bf924e07ee
Kevin Triplett <kt@kevintriplett.com>
2023-01-05 19:26:58 -0600

    [bug] allow non-sessioned users in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,8 +9,8 @@ module SessionsHelper
     user = User.find_by_token(params[:token])
     return unless user
     cookies.permanent.encrypted[:session_token] = user.generate_session_token
-    cookies.permanent[:user_name] = @current_user.name
-    cookies.permanent[:user_id] = @current_user.id
+    cookies.permanent[:user_name] = user.name
+    cookies.permanent[:user_id] = user.id
   end
 
   def sign_out
@@ -27,8 +27,8 @@ module SessionsHelper
     session_token = verify_and_decrypt_cookie(:session_token)
     @current_user ||= User.find_by_session_token(session_token)
     # TODO: remove this after January 2022
-    cookies.permanent[:user_name] = @current_user.name if @current_user
-    cookies.permanent[:user_id] = @current_user.id if @current_user
+    cookies.permanent[:user_name] = @current_user.name
+    cookies.permanent[:user_id] = @current_user.id
     @current_user
   end
 
