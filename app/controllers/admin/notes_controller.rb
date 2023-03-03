b7d702fced739b465ec4432aa88d9951bee13148
Kevin Triplett <kt@kevintriplett.com>
2023-03-02 20:55:32 -0500

    make notes larger temporary

--- app/controllers/admin/notes_controller.rb
+++ app/controllers/admin/notes_controller.rb
@@ -13,6 +13,7 @@ module Admin
       last_group = @survey.last_note_survey_group
       @template = Note.new(survey_group_id: last_group.id) if last_group
       @notes.last.update(updated_at: Time.now) unless @notes.empty? # cheat to enable live view
+      @body_class = "admin"
     end
 
     def create
