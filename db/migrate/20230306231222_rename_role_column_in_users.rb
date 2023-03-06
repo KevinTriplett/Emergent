e08958a06cd26e4194bcd6119f5f6aa4886c0ff1
Kevin Triplett <kt@kevintriplett.com>
2023-03-06 18:17:52 -0500

    postgresql dsoes not have rolws coluns

--- db/migrate/20230306231222_rename_role_column_in_users.rb
+++ db/migrate/20230306231222_rename_role_column_in_users.rb
@@ -1,9 +1,13 @@
 class RenameRoleColumnInUsers < ActiveRecord::Migration[7.0]
-  def change
+  def up
     if User.method_defined? :roles
       rename_column :users, :roles, :access_roles
     else
       add_column :users, :access_roles, :string
     end
+  end
+
+  def down
+    rename_column :users, :access_roles, :roles
   end
 end
