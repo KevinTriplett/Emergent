d181ae8310873910364123bfa45467bb54ac8c26
Kevin Triplett <kt@kevintriplett.com>
2023-03-06 18:16:52 -0500

    postgresql does not have roles

--- db/migrate/20230306231222_rename_role_column_in_users.rb
+++ db/migrate/20230306231222_rename_role_column_in_users.rb
@@ -1,5 +1,9 @@
 class RenameRoleColumnInUsers < ActiveRecord::Migration[7.0]
   def change
-    rename_column :users, :roles, :access_roles
+    if User.method_defined? :roles
+      rename_column :users, :roles, :access_roles
+    else
+      add_column :users, :access_roles, :string
+    end
   end
 end
