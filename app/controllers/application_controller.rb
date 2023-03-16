3820623eeab40bc42efabe3863e91ad8de6d3987
Kevin Triplett <kt@kevintriplett.com>
2023-03-16 09:55:50 -0700

    move cache busting headers to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :set_headers
+
+  def set_headers
+    headers["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
+    headers["Pragma"] = "no-cache" # HTTP 1.0.
+    headers["Expires"] = "0" # Proxies.
+  end
 end
