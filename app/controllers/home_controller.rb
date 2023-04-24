5797a01364499f826cf0e642e2bfa0dbe07f45cf
Kevin Triplett <kt@kevintriplett.com>
2023-04-23 21:31:29 -0400

    improve msg to be patient for magic links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,7 +32,7 @@ class HomeController < ApplicationController
           url = login_url(token: user.token, protocol: "https")
           Spider.append_message("magic_link_spider", "#{url}|#{user.id}")
         end
-        flash[:notice] = "Magic link sent, check your email SPAM folder and your Emergent Commons chat channel"
+        flash[:notice] = "Magic link sent, check your email SPAM folder and your Emergent Commons chat channel (Please be patient, it can take up to five minutes to receive the link)"
       end
       return redirect_to root_url
     end
