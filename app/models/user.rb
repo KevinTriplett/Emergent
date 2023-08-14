68716787155247e43b4440e5569ef5469a957aa5
Kevin Triplett <kt@kevintriplett.com>
2023-08-14 14:27:24 -0500

    modify user import rake task

--- app/models/user.rb
+++ app/models/user.rb
@@ -111,7 +111,7 @@ class User < ActiveRecord::Base
 
     # check first row for correct file format
     headers = data.shift
-    correct_headers = %w{first_name last_name email member_id join_date join_timestamp location time_zone country}.join("\t")
+    correct_headers = "Member ID,First Name,Last Name,Email Address"
     unless headers == correct_headers
       puts "header row = #{headers}" # first line is headers
       puts "should be  = #{correct_headers}"
@@ -119,42 +119,32 @@ class User < ActiveRecord::Base
     end
 
     data.each do |line|
-      first_name, last_name, email, member_id, x, join_timestamp, location, time_zone, country = line.split("\t")
-      location = location.gsub(/\"/, "") if location
+      member_id, first_name, last_name, email = line.split("\t")
       name = "#{first_name} #{last_name}"
-      profile_url = "https://emergent-commons.mn.co/members/#{member_id}"
-      chat_url = "https://emergent-commons.mn.co/chats/new?user_id=#{member_id}"
+      profile_url = member_id.present? ? "https://emergent-commons.mn.co/members/#{member_id}" : nil
+      chat_url = member_id.present? ? "https://emergent-commons.mn.co/chats/new?user_id=#{member_id}" : nil
 
       user = email.present? ? find_by_email(email) : find_by_name(name)
       if user
         puts "updating #{name}"
-        user = find_by_email(email)
         user.update(name: name)
         user.update(first_name: first_name)
         user.update(last_name: last_name)
-        user.update(email: email.downcase)
-        user.update(member_id: member_id)
-        user.update(profile_url: profile_url)
-        user.update(chat_url: chat_url)
-        user.update(join_timestamp: join_timestamp)
-        user.update(location: location)
-        user.update(country: country)
-        user.update(time_zone: time_zone)
+        user.update(email: email.downcase) if email.present?
+        user.update(member_id: member_id) if member_id.present?
+        user.update(profile_url: profile_url) if profile_url.present?
+        user.update(chat_url: chat_url) if chat_url.present?
       else
         puts "creating #{name}"
         create({
           name: name,
           first_name: first_name,
           last_name: last_name,
-          email: email.downcase,
+          email: (email || "").downcase,
           member_id: member_id,
           profile_url: profile_url,
           chat_url: chat_url,
-          status: "existing",
-          join_timestamp: join_timestamp,
-          location: location,
-          country: country,
-          time_zone: time_zone
+          status: "existing"
         })
       end
     end
