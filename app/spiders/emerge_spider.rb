c918d0dc0866a11cb265010e5be1d953f3828fa0
Kevin Triplett <kt@kevintriplett.com>
2023-09-27 14:56:36 -0500

    switch to firefox spider browser

--- app/spiders/emerge_spider.rb
+++ app/spiders/emerge_spider.rb
@@ -7,7 +7,7 @@ class EmergeSpider < Kimurai::Base
   end
   
   USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-  @@engine = Rails.env.development? ? :selenium_firefox : :selenium_chrome
+  @@engine = :selenium_firefox # :selenium_chrome
   @@urls = ["https://emergent-commons.mn.co/sign_in"]
   @@config = {
     user_agent: USER_AGENT,
