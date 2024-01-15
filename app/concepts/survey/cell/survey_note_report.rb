21e31e82300f99d1cf92feb19cbe5c623cf03eb8
Kevin Triplett <kt@kevintriplett.com>
2024-01-15 13:53:28 -0500

    more vote details

--- app/concepts/survey/cell/survey_note_report.rb
+++ app/concepts/survey/cell/survey_note_report.rb
@@ -10,10 +10,18 @@ class Survey::Cell::SurveyNoteReport < Cell::ViewModel
   def notes_ranked_with_votes
     survey_questions.collect do |sq|
       survey_answers = SurveyAnswer.where(survey_question_id: sq.id)
-      vote_counts = survey_answers.collect(&:vote_count)
+      vote_counts = survey_answers.collect(&:vote_count).select {|vc| vc > 0}
+      zeros_count = survey_answers.count - vote_counts.count
+      voters_count = vote_counts.count
       vote_counts.sort! { |a, b| a <=> b } # low to high
       total_votes = survey_answers.sum(&:vote_count)
-      {note: sq.note, votes: total_votes, counts: vote_counts}
+      {
+        note: sq.note,
+        votes: total_votes,
+        counts: vote_counts,
+        zeros: zeros_count,
+        voters: voters_count
+      }
     end.sort! { |a, b| b[:votes] <=> a[:votes] } # high to low
   end
 end
