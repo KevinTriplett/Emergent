7c4f9d402410c6fcf8b46cf96446d37a36b9179f
Kevin Triplett <kt@kevintriplett.com>
2024-01-15 13:40:42 -0500

    add details on note votes

--- app/concepts/survey/cell/survey_note_report.rb
+++ app/concepts/survey/cell/survey_note_report.rb
@@ -10,8 +10,9 @@ class Survey::Cell::SurveyNoteReport < Cell::ViewModel
   def notes_ranked_with_votes
     survey_questions.collect do |sq|
       survey_answers = SurveyAnswer.where(survey_question_id: sq.id)
+      vote_counts = survey_answers.collect(&:vote_count)
       total_votes = survey_answers.sum(&:vote_count)
-      {note: sq.note, votes: total_votes}
+      {note: sq.note, votes: total_votes, counts: vote_counts}
     end.sort! do |a, b|
       b[:votes] <=> a[:votes]
     end
