f19c23dbdc09879a8552ae99f4dd6e56ad3ba57b
Kevin Triplett <kt@kevintriplett.com>
2024-02-04 23:14:18 -0600

    simplify matching url in email

--- app/mailboxes/notifications_mailbox.rb
+++ app/mailboxes/notifications_mailbox.rb
@@ -5,13 +5,8 @@ class NotificationsMailbox < ApplicationMailbox
     file.write(mail.body.decoded)
     file.close
 
-    # get link to comment or post
-    match = mail.body.decoded.match(/See Comment: (https:\/\/mightynetworks.com.+?)\?/)
-    url = match[1] if match
-    match = mail.body.decoded.match(/See Post: (https:\/\/mightynetworks.com.+?)\?/)
-    url = match[1] if match
-
-    # ignore if no url
-    ModerationAssessment.create(url: url) if url
+    # get link to comment or post or article
+    match = mail.body.decoded.match(/: (https:\/\/mightynetworks.com.+?)\?/)
+    ModerationAssessment.create(url: match[1]) if match
   end
 end
