99855d9b8fe344d0986c82d2e241058e974e86fd
Kevin Triplett <kt@kevintriplett.com>
2024-02-04 22:45:09 -0600

    debugging mail processing

--- app/mailboxes/notifications_mailbox.rb
+++ app/mailboxes/notifications_mailbox.rb
@@ -2,6 +2,9 @@ require 'nokogiri'
 
 class NotificationsMailbox < ApplicationMailbox
   def process
+    file = File.new("last_email", "w")
+    file.write(mail.body.decoded)
+    file.close
     doc = Nokogiri::HTML(mail.body.decoded)
     # get link to comment or post
     url = doc.css('table.button-action-container a')[0].attribute_nodes[1].value.split('?')[0]
